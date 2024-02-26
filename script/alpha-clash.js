ffdab5ab6be2b26823e47ffc62c28dc3d7033d93
Tamanna5092 <tamannaaktermoni51@gmail.com>
2024-02-27 01:58:30 +0600

    keyboard start

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,11 +1,56 @@
-function play(){
-    // hide the home scree
-    const homeScreen = document.getElementById('home-screen');
-    homeScreen.classList.add('hidden');
-    // console.log(homeScreen)
-    // show the play ground
+// function play(){
+//     // hide the home scree
+//     const homeScreen = document.getElementById('home-screen');
+//     homeScreen.classList.add('hidden');
+//     // console.log(homeScreen)
+//     // show the play ground
 
-    const playGround = document.getElementById('play-ground');
-    // console.log(playGround.classList)
-    playGround.classList.remove('hidden');
+//     const playGround = document.getElementById('play-ground');
+//     // console.log(playGround.classList)
+//     playGround.classList.remove('hidden');
+// }
+
+function handelKeyboardButtonPress(event){
+    const playerPress = event.key
+    // console.log('player press', playerPress);
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    // console.log(playerPress, expectedAlphabet);
+
+    // if right or wrong key press
+    if(playerPress === expectedAlphabet){
+        console.log('you win a point');
+        console.log('you have pressed correctly', expectedAlphabet);
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('you missed. you lost a life')
+    }
 }
+// capture keyboard key press
+document.addEventListener('keyup',handelKeyboardButtonPress);
+
+function continueGame(){
+    // generate a random alphabet
+    const alphabet = getRandomAlphabet();
+    // console.log('your alphabet', alphabet)
+
+    // set randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText =alphabet;
+    
+    // set background color 
+    setBackgroundColorById(alphabet);
+}
+
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
+}
+
--- script/utility.js
+++ script/utility.js
@@ -1,4 +1,39 @@
-function getElementById(elementId){
-    const element = getElementById(elementId);
-    element.classList.add('hidden')
+function hideElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+function showElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden')
+}
+
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-amber-500')
+}
+
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-amber-500')
+}
+
+
+function getRandomAlphabet(){
+    // get or create alphabet array
+
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets)
+
+    // get a random index between 0 to 25
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+    // console.log(index);
+
+    const alphabet = alphabets[index];
+    // console.log(index, alphabet);
+    return alphabet;
 }
